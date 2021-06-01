daf3746f9ba7a3ccf145e609c24f97ab371e80cb
yuma wada <yuma.wada.1127@gmail.com>
2021-06-01 17:43:14 +0900

    Create CompleteTodos's Component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React, { useState } from "react";
 import { InputTodo } from "./components/InputTodo";
 import { IncompleteTodos } from "./components/IncompleteTodos";
+import { CompleteTodos } from "./components/CompleteTodos";
 
 export const App = () => {
   const [todoText, setTodoText] = useState("");
   const [incompleteTodos, setIncompleteTodos] = useState([]);
+  const [completeTodos, setCompleteTodos] = useState([]);
 
   const onChangeTodoText = (event) => setTodoText(event.target.value);
   const onClickCreateButton = () => {
@@ -14,6 +16,7 @@ export const App = () => {
   };
   const onClickCompleteButton = () => alert("完了ボタン押された");
   const onClickDeleteButton = () => alert("削除ボタン押された");
+  const onClickBackButton = () => alert("戻すボタンが押された");
 
   return (
     <>
@@ -26,6 +29,10 @@ export const App = () => {
         todos={incompleteTodos}
         onClickCompleteButton={onClickCompleteButton}
         onClickDeleteButton={onClickDeleteButton}
+      />
+      <CompleteTodos
+        todos={completeTodos}
+        onClickBackButton={onClickBackButton}
       />
     </>
   );
---
+++ src/components/CompleteTodos.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+
+export const CompleteTodos = (props) => {
+  const { todos, onClickBackButton } = props;
+
+  return (
+    <>
+      <h2>完了済のTodo</h2>
+      <ul>
+        {todos.map((todo, index) => {
+          return (
+            <li key={index}>
+              <p>{todo}</p>
+              <button onClick={onClickBackButton}>戻す</button>
+            </li>
+          );
+        })}
+      </ul>
+    </>
+  );
+};
